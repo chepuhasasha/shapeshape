c482d41520d935968fe43a3c0c208aed80ad735f
Sergey Batukov <sashachepuha@gmail.com>
2021-12-20 10:04:30 +0000

    update

--- src/dev.js
+++ src/dev.js
@@ -1,36 +1,43 @@
-import SS from './index';
+import ShapeShape from './index';
+// import SS from './index';
 
 const element = document.createElement('div');
 document.body.appendChild(element);
 
-const svg = SS.SVG({
-  id: '123',
-  width: 300,
-  height: 300,
-  container: element
-})
+const config = {
+  container: element,
+  props: {
+    width: 300,
+    height: 300,
+  },
+  childs: [
+    {
+      type: 'g',
+      props: {},
+      childs: [
+        {
+          type: 'circle',
+          props: {
+            cx: 65,
+            cy: 65,
+            r: 20,
+            fill: 'blue'
+          }
+        },
+        {
+          type: 'circle',
+          props: {
+            cx: 45,
+            cy: 45,
+            r: 40,
+            fill: 'red'
+          }
+        }
+      ]
+    }
+  ]
+}
 
-const circle = SS.circle({
-  cx: 45,
-  cy: 45,
-  r: 40,
-  fill: 'red'
-})
-const circle2 = SS.circle({
-  cx: 65,
-  cy: 65,
-  r: 20,
-  fill: 'blue'
-})
-
-
-svg.add(circle)
-svg.add(circle2)
-
-circle2.fill = 'blue'
-console.log(circle2)
-
-// svg.draw()
-// svg.update()
-// svg.parseJSON()
-// svg.parseSVG()
+const SS = new ShapeShape()
+console.log(SS)
+SS.parse(config)
--- src/index.js
+++ src/index.js
@@ -1,43 +1,67 @@
 export default class ShapeShape {
-  constructor(el) {
-    this.container = el;
+  constructor() {
     this.svgns = "http://www.w3.org/2000/svg"
   }
 
-  static SVG(props) {
-    return new SVG(props);
-  }
-
-  static circle(props) {
-    return new Element('circle', props)
-  }
-}
-
-class SVG {
-  constructor(props) {
-    this.elements = [];
-    this.svgns = "http://www.w3.org/2000/svg"
+  parse(config) {
     this.svg = document.createElementNS(this.svgns,'svg');
-    this.svg.setAttributeNS(null, "width", props.width);
-    this.svg.setAttributeNS(null, "height", props.height);
-    this.container = props.container
+    this.svg.setAttributeNS(null, "width", config.props.width);
+    this.svg.setAttributeNS(null, "height", config.props.height);
+    this.container = config.container
     this.container.appendChild(this.svg);
   }
-
-  add(el) {
-    this.elements.push(el.element)
-    this.svg.appendChild(el.element)
-  }
 }
 
-class Element {
-  constructor(name, props) {
-    this.fill = 'red',
-    this.svgns = "http://www.w3.org/2000/svg"
-    this.element = document.createElementNS(this.svgns, name)
-    Object.keys(props).forEach(key => {
-      this.element.setAttributeNS(null, key, props[key]);
-    })
-    return this
-  }
-}
+// export default class ShapeShape {
+//   constructor(el) {
+//     this.container = el;
+//     this.svgns = "http://www.w3.org/2000/svg"
+//   }
+
+//   static SVG(props) {
+//     return new SVG(props);
+//   }
+
+//   static circle(props) {
+//     return new Element('circle', props)
+//   }
+// }
+
+// class SVG {
+//   constructor(props) {
+//     this.elements = [];
+//     this.svgns = "http://www.w3.org/2000/svg"
+//     this.svg = document.createElementNS(this.svgns,'svg');
+//     this.svg.setAttributeNS(null, "width", props.width);
+//     this.svg.setAttributeNS(null, "height", props.height);
+//     this.container = props.container
+//     this.container.appendChild(this.svg);
+//   }
+
+//   add(el) {
+//     this.elements.push(el.element)
+//     this.svg.appendChild(el.element)
+//   }
+// }
+
+// class Element {
+//   constructor(name, props) {
+//     this.svgns = "http://www.w3.org/2000/svg"
+//     this.element = document.createElementNS(this.svgns, name)
+//     Object.keys(props).forEach(key => {
+//       this.element.setAttributeNS(null, key, props[key]);
+//     })
+//     return this
+//   }
+// }
+
+// class SVGProxy{
+// 	constructor(target){
+// 		return new Proxy(target,this);
+// 	}
+// 	get(target,prop){
+// 		return target[prop];
+// 	}
+// }
+
+
