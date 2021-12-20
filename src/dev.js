44ea40ec45a9bbcdf217a1737feaa725de873607
Sergey Batukov <sashachepuha@gmail.com>
2021-12-20 11:36:12 +0000

    update

--- src/dev.js
+++ src/dev.js
@@ -1,43 +1,7 @@
 import ShapeShape from './index';
-// import SS from './index';
 
 const element = document.createElement('div');
 document.body.appendChild(element);
 
-const config = {
-  container: element,
-  props: {
-    width: 300,
-    height: 300,
-  },
-  childs: [
-    {
-      type: 'g',
-      props: {},
-      childs: [
-        {
-          type: 'circle',
-          props: {
-            cx: 65,
-            cy: 65,
-            r: 20,
-            fill: 'blue'
-          }
-        },
-        {
-          type: 'circle',
-          props: {
-            cx: 45,
-            cy: 45,
-            r: 40,
-            fill: 'red'
-          }
-        }
-      ]
-    }
-  ]
-}
-
-const SS = new ShapeShape()
+const SS = new ShapeShape(element)
 console.log(SS)
-SS.parse(config)
--- src/index.js
+++ src/index.js
@@ -1,67 +1,29 @@
 export default class ShapeShape {
-  constructor() {
-    this.svgns = "http://www.w3.org/2000/svg"
+  constructor(el) {
+    this.init(el)
   }
 
-  parse(config) {
-    this.svg = document.createElementNS(this.svgns,'svg');
-    this.svg.setAttributeNS(null, "width", config.props.width);
-    this.svg.setAttributeNS(null, "height", config.props.height);
-    this.container = config.container
-    this.container.appendChild(this.svg);
+  init(el) {
+    this._svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+    this._parent = el
+    this._parent.appendChild(this._svg);
   }
 }
 
+
+
 // export default class ShapeShape {
-//   constructor(el) {
-//     this.container = el;
+//   constructor() {
 //     this.svgns = "http://www.w3.org/2000/svg"
 //   }
 
-//   static SVG(props) {
-//     return new SVG(props);
-//   }
-
-//   static circle(props) {
-//     return new Element('circle', props)
-//   }
-// }
-
-// class SVG {
-//   constructor(props) {
-//     this.elements = [];
-//     this.svgns = "http://www.w3.org/2000/svg"
+//   parse(config) {
 //     this.svg = document.createElementNS(this.svgns,'svg');
-//     this.svg.setAttributeNS(null, "width", props.width);
-//     this.svg.setAttributeNS(null, "height", props.height);
-//     this.container = props.container
+//     this.svg.setAttributeNS(null, "width", config.props.width);
+//     this.svg.setAttributeNS(null, "height", config.props.height);
+//     this.container = config.container
 //     this.container.appendChild(this.svg);
 //   }
-
-//   add(el) {
-//     this.elements.push(el.element)
-//     this.svg.appendChild(el.element)
-//   }
-// }
-
-// class Element {
-//   constructor(name, props) {
-//     this.svgns = "http://www.w3.org/2000/svg"
-//     this.element = document.createElementNS(this.svgns, name)
-//     Object.keys(props).forEach(key => {
-//       this.element.setAttributeNS(null, key, props[key]);
-//     })
-//     return this
-//   }
-// }
-
-// class SVGProxy{
-// 	constructor(target){
-// 		return new Proxy(target,this);
-// 	}
-// 	get(target,prop){
-// 		return target[prop];
-// 	}
 // }
 
 
