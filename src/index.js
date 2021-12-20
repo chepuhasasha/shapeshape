44a59c62676a18a710c24328d342b08cb64d2488
Sergey Batukov <sashachepuha@gmail.com>
2021-12-20 13:03:47 +0000

    update

--- src/index.js
+++ src/index.js
@@ -27,9 +27,9 @@ class SVG {
   }
 }
 
-class Circle {
-  constructor(props = {}) {
-    this._element = document.createElementNS('http://www.w3.org/2000/svg','circle');
+class Element {
+  constructor(name, props = {}) {
+    this._element = document.createElementNS('http://www.w3.org/2000/svg', name);
     Object.keys(props).forEach(key => {
       this._element.setAttributeNS(null, key, props[key]);
     })
@@ -47,53 +47,27 @@ class Circle {
 export default {
   SVG(el, props = {}) {
     const svg = new SVG(el, props)
-    return new Proxy(svg, {
-      get: function(item, property){
-        console.log(`get: '${property}: ${item[property]}'`);
-        svg.update();
-        return item[property];
-      },
-      set: function(item, property, val){
-        console.log(`set: '${property}: ${val}'`);
-        item[property] = val
-        svg.update();
-        return item[property];
-      }
-    });
+    return proxying(svg)
   },
 
   circle(props = {}) {
-    const circle = new Circle(props)
-    return new Proxy(circle, {
-      get: function(item, property){
-        console.log(`get: '${property}: ${item[property]}'`);
-        circle.update();
-        return item[property];
-      },
-      set: function(item, property, val){
-        console.log(`set: '${property}: ${val}'`);
-        item[property] = val
-        circle.update();
-        return item[property];
-      }
-    });
+    const circle = new Element('circle', props)
+    return proxying(circle)
   }
 }
 
-
-
-// export default class ShapeShape {
-//   constructor() {
-//     this.svgns = "http://www.w3.org/2000/svg"
-//   }
-
-//   parse(config) {
-//     this.svg = document.createElementNS(this.svgns,'svg');
-//     this.svg.setAttributeNS(null, "width", config.props.width);
-//     this.svg.setAttributeNS(null, "height", config.props.height);
-//     this.container = config.container
-//     this.container.appendChild(this.svg);
-//   }
-// }
-
-
+const proxying = function(target) {
+  return new Proxy(target, {
+    get: function(item, property){
+      console.log(`get: '${property}: ${item[property]}'`);
+      target.update();
+      return item[property];
+    },
+    set: function(item, property, val){
+      console.log(`set: '${property}: ${val}'`);
+      item[property] = val
+      target.update();
+      return item[property];
+    }
+  });
+}
