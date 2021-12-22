b1aca8c3a97a47f33856306d7c558a2a21c4ddba
Sergey Batukov <sashachepuha@gmail.com>
2021-12-22 08:53:06 +0000

    update

--- src/dev.js
+++ src/dev.js
@@ -3,33 +3,10 @@ import SS from './index';
 const container = document.createElement('div');
 document.body.appendChild(container);
 
-const svg = SS.SVG({
-  id: 'svg',
-  width: 500,
-  height: 500,
-}, container)
-
-const r = SS.rect({
-  x: 0,
-  y: 0,
-  width: 200,
-  height: 100,
-  fill: 'red'
-})
-
-const c = SS.circle({
-  cx: 200,
-  cy: 100,
-  r: 50,
-  fill: 'blue'
-})
-
-const c2 = SS.circle({
-  cx: 100,
-  cy: 100,
-  r: 50,
-  fill: 'green'
-})
+const svg = SS.SVG({ id: 'svg', width: 500, height: 500 }, container)
+const r = SS.rect({ x: 0, y: 0, width: 200, height: 100, fill: 'red' })
+const c = SS.circle({ cx: 200, cy: 100, r: 50, fill: 'blue' })
+const c2 = SS.circle({ cx: 100, cy: 100, r: 50, fill: 'green' })
 
 r.fill = 'blue'
 c.fill = 'red'
